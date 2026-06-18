1caa865c06edef74fd3b2ff1edba42af12866f56
navdhasharma02 <navdhasharma02@gmail.com>
2026-06-18 22:08:17 +0530

    fis:fixed deployment issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 import { env } from "./lib/env.js";
 
-app.listen(env.PORT, "127.0.0.1", () => {
-  console.log(`Easy_Resume API listening on http://127.0.0.1:${env.PORT}`);
+app.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`Easy_Resume API listening on port ${env.PORT}`);
 });
