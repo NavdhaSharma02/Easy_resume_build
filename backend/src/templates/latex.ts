74a2329befc1e70876be243e2f264946c58ae600
navdhasharma02 <navdhasharma02@gmail.com>
2026-06-20 11:26:32 +0530

    fix : fixed size

--- backend/src/templates/latex.ts
+++ backend/src/templates/latex.ts
@@ -181,7 +181,7 @@ export function generateLatex(data: ResumeData, template: TemplateId) {
 % License : MIT
 %------------------------
 
-\\documentclass[letterpaper,11pt]{article}
+\\documentclass[letterpaper,10pt]{article}
 
 \\usepackage{latexsym}
 \\usepackage[empty]{fullpage}
@@ -196,6 +196,8 @@ export function generateLatex(data: ResumeData, template: TemplateId) {
 \\usepackage{tabularx}
 \\usepackage{fontawesome5}
 
+\\linespread{0.94}
+
 \\definecolor{light-grey}{gray}{0.83}
 \\definecolor{dark-grey}{gray}{0.3}
 \\definecolor{text-grey}{gray}{.08}
@@ -209,55 +211,60 @@ export function generateLatex(data: ResumeData, template: TemplateId) {
 \\renewcommand{\\headrulewidth}{0pt}
 \\renewcommand{\\footrulewidth}{0pt}
 
-\\addtolength{\\oddsidemargin}{-0.5in}
+\\addtolength{\\oddsidemargin}{-0.65in}
 \\addtolength{\\evensidemargin}{0in}
-\\addtolength{\\textwidth}{1in}
-\\addtolength{\\topmargin}{-.5in}
-\\addtolength{\\textheight}{1.0in}
+\\addtolength{\\textwidth}{1.3in}
+\\addtolength{\\topmargin}{-.7in}
+\\addtolength{\\textheight}{1.35in}
 
 \\urlstyle{same}
 \\raggedbottom
 \\raggedright
 \\setlength{\\tabcolsep}{0in}
+\\setlength{\\parskip}{0pt}
+\\setlength{\\parsep}{0pt}
+\\sloppy
+\\emergencystretch=2em
 
 \\titleformat {\\section}{
-    \\bfseries \\vspace{2pt} \\raggedright \\large
-}{}{0em}{}[\\color{light-grey} {\\titlerule[2pt]} \\vspace{-4pt}]
+    \\bfseries \\vspace{-4pt} \\raggedright \\normalsize
+}{}{0em}{}[\\color{light-grey} {\\titlerule[1pt]} \\vspace{-9pt}]
 
 \\newcommand{\\resumeItem}[1]{
   \\item\\small{
-    {#1 \\vspace{-1pt}}
+    {#1 \\vspace{-4pt}}
   }
 }
 
 \\newcommand{\\resumeSubheading}[4]{
-  \\vspace{-1pt}\\item
+  \\vspace{-4pt}\\item
     \\begin{tabular*}{\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}
-      \\textbf{#1} & {\\color{dark-grey}\\small #2}\\vspace{1pt}\\\\
+      \\textbf{#1} & {\\color{dark-grey}\\small #2}\\vspace{-1pt}\\\\
       \\textit{#3} & {\\color{dark-grey} \\small #4}\\\\
-    \\end{tabular*}\\vspace{-4pt}
+    \\end{tabular*}\\vspace{-8pt}
 }
 
 \\newcommand{\\resumeProjectHeading}[2]{
     \\item
     \\begin{tabular*}{\\textwidth}{l@{\\extracolsep{\\fill}}r}
       #1 & {\\color{dark-grey}\\small #2} \\\\
-    \\end{tabular*}\\vspace{-1pt}
+    \\end{tabular*}\\vspace{-5pt}
 }
 
 \\renewcommand\\labelitemii{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}
-\\newcommand{\\resumeSubHeadingListStart}{\\begin{itemize}[leftmargin=0in, label={}]}
+\\newcommand{\\resumeSubHeadingListStart}{\\begin{itemize}[leftmargin=0in, label={}, itemsep=0pt, topsep=0pt, parsep=0pt, partopsep=0pt]}
 \\newcommand{\\resumeSubHeadingListEnd}{\\end{itemize}}
-\\newcommand{\\resumeItemListStart}{\\begin{itemize}}
-\\newcommand{\\resumeItemListEnd}{\\end{itemize}\\vspace{0pt}}
+\\newcommand{\\resumeItemListStart}{\\begin{itemize}[itemsep=0pt, topsep=0pt, parsep=0pt, partopsep=0pt]}
+\\newcommand{\\resumeItemListEnd}{\\end{itemize}\\vspace{-6pt}}
 
 \\color{text-grey}
 
 \\begin{document}
+\\small
 
 %----------HEADING----------
 \\begin{center}
-    \\textbf{\\Huge ${latexText(data.personal.fullName || "Your Name")}} \\\\ \\vspace{5pt}
+    \\textbf{\\Large ${latexText(data.personal.fullName || "Your Name")}} \\\\ \\vspace{1pt}
 ${headerContactLine(data)}
 \\end{center}
 
